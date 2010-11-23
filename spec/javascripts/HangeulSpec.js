2bcfb3fcd29cdf35ebdb8fef6f7e0f6905f196df
Ben Lickly <blickly@eecs>
2010-11-23 15:14:45 -0800

    Changed single jamo checks to compatibility jamo.
    This is more consistent with what a keyboard will
    generate, and seems to play nicer with other
    applications.  As a slight downside, this means
    that my code won't be able to render correctly in
    case I break up characters into single jamo
    incorrectly, which it can do when using regular
    jamo. Since I plan on having my application break
    up the characters correctly, however, I won't
    worry about this.

--- spec/javascripts/HangeulSpec.js
+++ spec/javascripts/HangeulSpec.js
@@ -5,20 +5,19 @@ describe("ascii2hangeul", function() {
     describe("made of a single jamo", function() {
 
       it("should be able to print a single 자음", function() {
-        expect(ascii2hangeul("r")).toEqual("ㄱ");
+        //expect(ascii2hangeul("r")).toEqual("ㄱ");
         //expect(ascii2hangeul("r")).toEqual("ᄀ");
         //var hangeul_consonants='ᄀᄁᄂᄃᄄᄅᄆᄇᄈᄉᄊᄋᄌᄍᄎᄏᄐᄑᄒ';
-        /*
         var ascii_jamo = 'qwertyuiopasdfghjklzxcvbnmQWERTOP';
-        var hangeul_jamo = 'ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔㅁㄴㅇㄹㅎㅗㅓㅏㅣㅋㅌㅍㅠㅜㅡㅃㅉㄸㄲㅆㅒㅖ';
+        var hangeul_compatibiltiy_jamo = 'ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔㅁㄴㅇㄹㅎㅗㅓㅏㅣㅋㅌㅊㅍ
+        var hangeul_jamo = 'ᄇᄌᄃᄀ쇼ᅧᅣᅢᅦᄆᄂᄋᄅ호ᅥᅡᅵᄏᄐᄎ퓨ᅮᅳᄈᄍᄄᄁ썌ᅨ';
 
         var l = ascii_jamo.length;
         for (var i = 0; i < l; i++) {
           ascii = ascii_jamo.charAt(i);
-          hangeul = hangeul_jamo.charAt(i);
+          hangeul = hangeul_compatibility_jamo.charAt(i);
           expect(ascii2hangeul(ascii)).toEqual(hangeul);
         }
-        */
       });
     });
 
